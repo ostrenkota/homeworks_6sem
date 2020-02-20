94587a5c45aa16f55626cdabfb7e4aa00ecc4769
ostrnkota <ostrenko.ta@gmail.com>
2020-02-20 17:02:16 +0300

    Task2 added

---
+++ task2/Task2/Goods.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+
+namespace Task2
+{
+    abstract class Goods
+    {
+        public Goods(string name, double costs)
+        {
+            this.Name = name;
+            this.Costs = costs;
+        }
+
+        public string Name { get; set; }
+        public double Costs { get; set; }
+        
+
+        public virtual string getInfo()
+        {
+           return JsonConvert.SerializeObject(this);
+        }
+        public abstract bool isFresh();
+    }
+
+    class Product : Goods
+    {  
+        public Product(string name, double costs, DateTime produceDate, DateTime freshUntill) :
+            base(name, costs)
+        {
+            this.ProduceDate = produceDate;
+            this.FreshUntill = freshUntill;
+        }
+
+        public DateTime ProduceDate { get; set; }
+        public DateTime FreshUntill { get; set; }
+
+        public override bool isFresh()
+        {
+            return DateTime.Compare(DateTime.Now, FreshUntill) <= 0;
+        }
+
+    }
+
+    class Consigment : Goods
+    {
+        public Consigment(int batch, string name, double costs, DateTime produceDate, DateTime freshUntill) :
+             base(name, costs)
+        {
+            this.Batch = batch;
+            this.ProduceDate = produceDate;
+            this.FreshUntill = freshUntill;
+        }
+
+        public int Batch;
+        public DateTime ProduceDate { get; set; }
+        public DateTime FreshUntill { get; set; }
+
+        public override bool isFresh()
+        {
+            return DateTime.Compare(DateTime.Now, FreshUntill) <= 0;
+        }
+    }
+
+    class Set : Goods
+    {
+        public Set(string name, double costs, Product[] products) :
+            base(name, costs)
+        {
+            this.Products = products;
+        }
+
+        public Product[] Products;
+
+        public override bool isFresh()
+        {
+            return Array.TrueForAll(Products, x => DateTime.Compare(DateTime.Now, x.FreshUntill) <= 0);
+        }
+    } 
+}
---
+++ task2/Task2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonInfo;
+            using (StreamReader sr = new StreamReader("../../../input.json"))
+            {
+                 jsonInfo = sr.ReadToEnd();
+            }
+
+            InputData info = JsonConvert.DeserializeObject<InputData>(jsonInfo);
+            List<Goods> goodsList = new List<Goods>();
+            goodsList.AddRange(info.Products);
+            goodsList.AddRange(info.Consigments);
+            goodsList.AddRange(info.Sets);
+            var goodsArray = goodsList.ToArray();
+
+            foreach(var elem in goodsArray)
+            {
+                Console.WriteLine(elem.getInfo());
+            }
+
+            Console.WriteLine("\n\nNot fresh goods list:\n");
+
+            foreach (var elem in findNotFreshGoods(goodsArray))
+            {
+                Console.WriteLine(elem.Name);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static Goods[] findNotFreshGoods(Goods[] goods)
+        {
+            List<Goods> notFreshist = new List<Goods>();
+            foreach (var elem in goods)
+            {
+                if (!elem.isFresh())
+                {
+                    notFreshist.Add(elem);
+                }
+            }
+
+            return notFreshist.ToArray();
+        }
+    }
+
+    class InputData
+    {
+        public int n;
+        public Product[] Products;
+        public Consigment[] Consigments;
+        public Set[] Sets;    
+    }
+}
