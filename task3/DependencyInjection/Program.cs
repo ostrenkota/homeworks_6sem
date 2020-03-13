9bc3383050bd5484232bcf07ffeb6b6667b48b2f
ostrnkota <ostrenko.ta@gmail.com>
2020-03-13 12:14:01 +0300

    Program logic added

--- task3/DependencyInjection/Program.cs
+++ task3/DependencyInjection/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Text.RegularExpressions;
 
 namespace DependencyInjection
@@ -17,27 +19,39 @@ namespace DependencyInjection
             }
             else
             {
-                DBWorker Worker = new DBWorker(dbType);
-                Worker.createDataBase("My cool DB");
+                Console.WriteLine("Введите название нужного метода (без аргументов)");
+                string methodName = Console.ReadLine();
 
-                Console.WriteLine("Введите список действий:");
-
-                string[] actions = Regex.Split(Console.ReadLine(), " ");
-
-                for (int i = 0; i < actions.Length; i++)
+                try
                 {
-                    switch (actions[i]) 
-                    {
-                        case "createDataBase":
-                            Worker.createDataBase("My cool DB");
-                            break;
+                    Type workerType = typeof(DBWorker);
+                    object workerInstance = Activator.CreateInstance(workerType, dbType);
+                    MethodInfo toInvoke = workerType.GetMethod(methodName);
 
+                    try
+                    {
+                        Console.WriteLine("Сколько будет аргументов?");
+                        int count = Int32.Parse(Console.ReadLine());
+
+                        object[] arguments = new string[count];
+                        Console.WriteLine("Введите аргументы:");
+                        for (int i = 0; i < count; i++)
+                        {
+                            arguments[i] = Console.ReadLine();
+                        }
+                        toInvoke.Invoke(workerInstance, arguments);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Неверно заданы параметры.");
                     }
                 }
+                catch
+                {
+                    Console.WriteLine("Данный метод не поддерживается.");
+                }
             }
-
             Console.ReadKey();
-
         }
     }
 }
