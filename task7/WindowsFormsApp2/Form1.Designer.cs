9e14aac43994f3ad8a88fa1d8cfd032710703d3c
ostrnkota <ostrenko.ta@gmail.com>
2020-04-16 17:57:46 +0300

    Task7 second part added

---
+++ task7/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(502, 64);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.MaximumSize = new System.Drawing.Size(500, 80);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(286, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 125;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar2.Location = new System.Drawing.Point(502, 177);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.MaximumSize = new System.Drawing.Size(500, 80);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(286, 56);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.Value = 125;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar3.Location = new System.Drawing.Point(502, 293);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.MaximumSize = new System.Drawing.Size(500, 80);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(286, 56);
+            this.trackBar3.TabIndex = 2;
+            this.trackBar3.Value = 125;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(125)))), ((int)(((byte)(125)))));
+            this.pictureBox1.Location = new System.Drawing.Point(43, 64);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(343, 285);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(343, 285);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(431, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Red:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(431, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Green:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(431, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Blue:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(508, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(743, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "255";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(510, 208);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.Location = new System.Drawing.Point(743, 208);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 25);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "255";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label8.Location = new System.Drawing.Point(510, 324);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 25);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label9.Location = new System.Drawing.Point(743, 324);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 25);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "255";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(81, 95);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 17);
+            this.label10.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 417);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.MinimumSize = new System.Drawing.Size(1000, 464);
+            this.Name = "Form1";
+            this.Text = "Color Picker";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ task7/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.pictureBox1.Width = this.Width / 2;
+            this.trackBar1.Width = this.Width / 4;
+            this.trackBar2.Width = this.Width / 4;
+            this.trackBar3.Width = this.Width / 4;
+            this.trackBar1.Location = new Point((this.Width * 2) / 3, this.Height / 6);
+            this.label1.Location = new Point(((this.Width * 2) / 3) - 50, this.Height / 6);
+            this.label4.Location = new Point(((this.Width * 2) / 3), this.Height / 6 + 30);
+            this.label5.Location = new Point(((this.Width * 2) / 3) + trackBar1.Width - 20, this.Height / 6 + 30);
+            this.trackBar2.Location = new Point((this.Width * 2) / 3, (this.Height / 3 + this.Height / 2) / 2);
+            this.label2.Location = new Point(((this.Width * 2) / 3) - 60, (this.Height / 3 + this.Height / 2) / 2);
+            this.label6.Location = new Point(((this.Width * 2) / 3), (this.Height / 3 + this.Height / 2) / 2 + 30);
+            this.label7.Location = new Point(((this.Width * 2) / 3) + trackBar2.Width - 20, (this.Height / 3 + this.Height / 2) / 2 + 30);
+            this.trackBar3.Location = new Point((this.Width * 2) / 3, (this.Height / 3 + this.Height) / 2);
+            this.label3.Location = new Point(((this.Width * 2) / 3) - 50, (this.Height / 3 + this.Height) / 2);
+            this.label8.Location = new Point(((this.Width * 2) / 3), (this.Height / 3 + this.Height) / 2 + 30);
+            this.label9.Location = new Point(((this.Width * 2) / 3) + trackBar3.Width - 20, (this.Height / 3 + this.Height) / 2 + 30);
+            Clipboard.SetText("#7D7D7D");
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value);
+            Clipboard.SetText("#" + Convert.ToString(trackBar1.Value, 16).ToUpper() + Convert.ToString(trackBar2.Value, 16).ToUpper() + Convert.ToString(trackBar3.Value, 16).ToUpper());
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value);
+            Clipboard.SetText("#" + Convert.ToString(trackBar1.Value, 16).ToUpper() + Convert.ToString(trackBar2.Value, 16).ToUpper() + Convert.ToString(trackBar3.Value, 16).ToUpper());
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value);
+            Clipboard.SetText("#" + Convert.ToString(trackBar1.Value, 16).ToUpper() + Convert.ToString(trackBar2.Value, 16).ToUpper() + Convert.ToString(trackBar3.Value, 16).ToUpper());
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            label10.Text = "#" + Convert.ToString(trackBar1.Value, 16).ToUpper() + Convert.ToString(trackBar2.Value, 16).ToUpper() + Convert.ToString(trackBar3.Value, 16).ToUpper();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.pictureBox1.Width = this.Width / 2;
+            this.trackBar1.Width = this.Width / 4;
+            this.trackBar2.Width = this.Width / 4;
+            this.trackBar3.Width = this.Width / 4;
+            this.trackBar1.Location = new Point((this.Width*2)/3, this.Height/6);
+            this.label1.Location = new Point(((this.Width * 2) / 3) - 50, this.Height / 6);
+            this.label4.Location = new Point(((this.Width * 2) / 3), this.Height / 6 + 30);
+            this.label5.Location = new Point(((this.Width * 2) / 3) + trackBar1.Width - 20, this.Height / 6 + 30);
+            this.trackBar2.Location = new Point((this.Width * 2) / 3, (this.Height / 3 + this.Height / 2) / 2);
+            this.label2.Location = new Point(((this.Width * 2) / 3) - 60, (this.Height / 3 + this.Height / 2) / 2);
+            this.label6.Location = new Point(((this.Width * 2) / 3), (this.Height / 3 + this.Height / 2) / 2 + 30);
+            this.label7.Location = new Point(((this.Width * 2) / 3) + trackBar2.Width - 20, (this.Height / 3 + this.Height / 2) / 2 + 30);
+            this.trackBar3.Location = new Point((this.Width * 2) / 3, (this.Height / 3 + this.Height) / 2);
+            this.label3.Location = new Point(((this.Width * 2) / 3) - 50, (this.Height / 3 + this.Height) / 2);
+            this.label8.Location = new Point(((this.Width * 2) / 3), (this.Height / 3 + this.Height) / 2 + 30);
+            this.label9.Location = new Point(((this.Width * 2) / 3) + trackBar3.Width - 20, (this.Height / 3 + this.Height) / 2 + 30);
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            label10.Text = "";
+        }
+    }
+}
